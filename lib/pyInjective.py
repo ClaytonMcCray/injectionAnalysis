2dfb4be51e2f03306252e3721b8d0d27b0bad365
Clayton McCray <cpmdmccray@gmail.com>
2018-12-16 01:25:44 -0600

    Added error raising

--- lib/pyInjective.py
+++ lib/pyInjective.py
@@ -1,17 +1,20 @@
-from ctypes import cdll
+from ctypes import CDLL
 from sys import platform
 from os import getcwd
 
 
 if platform == "linux":
-    lib = cdll.LoadLibrary(getcwd() + '/lib/bin/linux-gnu/injection.so')
+    lib = CDLL(getcwd() + '/lib/bin/linux-gnu/injection.so')
 elif platform == "darwin":
-    lib = cdll.LoadLibrary(getcwd() + '/lib/bin/darwin17/injection.so')
+    lib = CDLL(getcwd() + '/lib/bin/darwin17/injection.so')
 
 
-class RandInjective(object):
-    def __init__(self):
-        self.obj = lib.randInjective_new()
+def injective(domain, codomain):
+    ret_val = lib.injective(domain, codomain)
+    if ret_val == 1:
+        return True
+    elif ret_val == 0:
+        return False
+    else:
+        raise RuntimeError
 
-    def injective(self, domain, codomain):
-        return lib.randInjective_injective(self.obj, domain, codomain) == 1
