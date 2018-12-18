600a7c187374fe5cb2751983bf4b92ca95e7186a
Clayton McCray <cpmdmccray@gmail.com>
2018-12-17 23:06:16 -0600

    created config file

---
+++ config.py
@@ -0,0 +1,17 @@
+# this should be a list of function names as strings
+tests_to_run = [
+        
+        ]
+
+
+# this should be a nested list of tuples of the parameters.
+# functions are indexed by tests_to_run
+test_params = [
+        ()
+        ]
+
+
+# this is a dictionary of global parameters
+global_param = {
+        
+        }
